3198e131f9cc9f664b45013e990723bd7f462b3f
Alaina Balducci <alainabalducci@gmail.com>
2023-03-23 13:37:47 -0500

    .

--- HelloASPDotNET/Controllers/HelloController.cs
+++ HelloASPDotNET/Controllers/HelloController.cs
@@ -2,60 +2,24 @@
 
 namespace HelloASPDotNET.Controllers
 {
-    [Route("/helloworld")]
+   
     public class HelloController : Controller
     {
         
-        // GET: /helloworld/
+        
         [HttpGet]
         public IActionResult Index()
         {
-            string html = "<form method='post' action='/helloworld/welcome'>" +
-            "<input type='text' name='name' />" +
-            "<select name='language'> <option value='Hello'>English</option> <option value='Bonjour'>French</option>\" <option value='Hola'>Spanish</option>\"<option value='Ni Hao'>Mandarin</option>\"<option value='Ciao'>Italian</option>\" </select>" +
-
-            "<input type='submit' value='Greet Me!' />" +
-            "</form>";
-            return Content(html, "text/html");
+            return View();
         }
-        //GET: /helloworld/welcome
-        //POST: /helloworld/welcome
-        //[HttpPost("welcome")]
-        //[HttpGet("welcome/{name?}")]
-        //public IActionResult Welcome(string name, string language)
-        //{
 
 
-        //    return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
-        //}
-        [HttpPost("welcome")]
-        [HttpGet("welcome/{name?}/{language?}")]
-        public IActionResult CreateMessage(string name = "Patrick", string language = "Sup")
+        [HttpPost("/hello")]
+
+        public IActionResult Welcome(string name)
         {
-
-           // string html = "<h1>{language}" + "{name}!</h1>";
-            return Content($"<h1>{language}, {name}!</h1>", "text/html");
-            //string hello = "";
-            //switch (language)
-            //{
-            //    case "English":
-            //        hello = "hello ";
-            //        break;
-            //    case "French":
-            //        hello = "Bonjour ";
-            //        break;
-            //    case "Spanish":
-            //        hello = "Hola ";
-            //        break;
-            //    case "Mandarin":
-            //        hello = "Ni Hao";
-            //        break;
-            //    case "Italian":
-            //        hello = "Ciao";
-            //        break;
-
-            //}
-            //return hello + name;
+            ViewBag.person = name;
+            return View();
         }
     }
 }
